d3e9ad722518805ba49c1d6f09ca666834397f5d
Víctor Alonso <works.alonsog@gmail.com>
2022-01-07 11:03:29 +0100

    check empty data

--- example-frontend.php
+++ example-frontend.php
@@ -4,6 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $photo_gallery = get_post_meta( get_the_ID(), 'gallery_data', true );
+if(!$photo_gallery){
+    $photo_gallery = array();
+    $photo_gallery['image_url'] = [];
+}
+
 foreach ($photo_gallery['image_url'] as $photo) {
     echo $photo;
 }
